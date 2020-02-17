9e2d761b5adda8c81d47dad6610f4f2c6447a705
UtkrishtaBhattarai <49023926+UtkrishtaBhattarai@users.noreply.github.com>
2020-02-17 19:54:18 +0545

    defining variable in python

--- app.py
+++ app.py
@@ -4,3 +4,18 @@ print("Hello world")
 print("0---")
 print("|||")
 print('*'*10)
+
+#defning variable in python
+numberfirst=1
+numbersecond=2
+print(numberfirst*numbersecond)
+
+#types of variable in python
+is_true=True
+price=990.9
+
+##on some hospital scenario
+name="John"
+age=20
+is_new=False
+
