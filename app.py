4c83ea04c9fdade23e373e38f1e13c8c5d2a70cd
UtkrishtaBhattarai <49023926+UtkrishtaBhattarai@users.noreply.github.com>
2020-02-17 20:12:49 +0545

    calculations and getting the type of the variable

--- app.py
+++ app.py
@@ -19,3 +19,20 @@ name="John"
 age=20
 is_new=False
 
+# name1=input("What is your name? ")
+# print("Hi"+" "+ name1)
+
+#asking person name and color and print like utkrishta likes black
+# name2=input("What is your name ? ")
+# color=input("What is your favourite color? ")
+# print("My name is "+ name2+" "+ "and my favourite color is"+" "+ color+".")
+
+# parsing into string
+#lets calculate the age
+
+displayage=input("What is your birth year?: ")
+calculateage=2020-int(displayage)
+print(calculateage)
+
+print(type(displayage))# used to print the variable type
+
